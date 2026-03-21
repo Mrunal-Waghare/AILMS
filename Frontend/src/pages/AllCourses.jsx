864610667522529c62a8d88e4811ff1f7e5d5970
Mrunal-Waghare <mrunalwaghare@gmail.com>
2026-03-21 14:12:00 +0530

    Fix import case sensitivity for Nav and SearchAi

--- Frontend/src/pages/AllCourses.jsx
+++ Frontend/src/pages/AllCourses.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { FaArrowLeft } from "react-icons/fa6";
-import Nav from '../component/nav';
+import Nav from '../component/Nav';
 import { useNavigate } from 'react-router-dom';
-import ai from "../assets/SearchAI.png"
+import ai from "../assets/SearchAi.png";
 import { useSelector } from 'react-redux';
 import Card from '../component/Card';
 
